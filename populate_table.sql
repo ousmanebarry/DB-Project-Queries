bbf4138aa5560a1f88f293c51aa8fed721f739e8
Rxmi-sk <Rslimk3@gmail.com>
2023-03-23 13:44:51 -0400

    added 5 hotel chains

---
+++ populate_table.sql
@@ -0,0 +1,16 @@
+USE hotel_management;
+
+INSERT INTO Hotel_Chain (Name, Address, Number_Of_Hotels, Contact_Email, Contact_Phone)
+VALUES ('Marriott', '157 Mariott road', '8', 'Mariott@gmail.com', '613-429-5006');
+
+INSERT INTO Hotel_Chain (Name, Address, Number_Of_Hotels, Contact_Email, Contact_Phone)
+VALUES ('Hilton', '382 Hilton Avenue', '8', 'Hilton@gmail.com', '613-486-3006');
+
+INSERT INTO Hotel_Chain (Name, Address, Number_Of_Hotels, Contact_Email, Contact_Phone)
+VALUES ('Wyndham', '182 Windy Road ', '8', 'Wyndham@gmail.com', '613-404-2906');
+
+INSERT INTO Hotel_Chain (Name, Address, Number_Of_Hotels, Contact_Email, Contact_Phone)
+VALUES ('Four Seasons', '287 Silver Way ', '8', '4Seasons@gmail.com', '613-356-4006');
+
+INSERT INTO Hotel_Chain (Name, Address, Number_Of_Hotels, Contact_Email, Contact_Phone)
+VALUES ('Cardinal', '839 Viking Road ', '8', 'Cardinal@gmail.com', '613-423-4006');
