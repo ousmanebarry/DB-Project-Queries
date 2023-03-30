2f507d0fec22d9c4dbabc1f794d3cad2f6e5afe7
Rxmi-sk <Rslimk3@gmail.com>
2023-03-30 17:32:42 -0400

    Added 3 more views

--- views.sql
+++ views.sql
@@ -22,7 +22,7 @@ FROM customer
 WHERE customer.Username;
 
 -- Create Available_Rooms view
-CREATE VIEW Available_Rooms_Area AS
+CREATE VIEW Available_Rooms_Area_View AS
 SELECT  TRIM(SUBSTRING_INDEX(h.Address, ' ', -2)) AS Area, COUNT(r.Room_ID) AS Num_Available_Rooms
 FROM Hotel h JOIN Room r ON h.Hotel_ID = r.Hotel_ID
   LEFT JOIN (
@@ -34,10 +34,35 @@ WHERE b.Room_ID IS NULL
 GROUP BY Area;
 
 -- Create Capacity_Of_Rooms_In_Hotel view
-CREATE VIEW Capacity_Of_Rooms_In_Hotel AS
+CREATE VIEW Capacity_Of_Rooms_In_Hotel_View AS
 SELECT 
 	Hotel.Hotel_ID,
     SUM(Room.Capacity) AS Total_Capacity_Of_Hotel_Rooms
 FROM Hotel JOIN Room ON Hotel.Hotel_ID = Room.Hotel_ID
 GROUP BY Hotel.Hotel_ID; 
-    
+
+-- Create Booking view
+CREATE VIEW Booking_View AS
+SELECT c.Full_name, b.First_Day, b.Last_Day, h.Chain_Name, h.Category, b.Price, ro.Room_Number
+FROM Booking b 
+JOIN Customer c ON c.Customer_ID = b.Customer_ID
+JOIN Room ro ON b.Room_ID = ro.Room_ID
+JOIN Hotel h ON b.Hotel_ID = h.Hotel_ID;
+
+-- Create Renting view
+CREATE VIEW Renting_View AS
+SELECT c.Full_name, b.First_Day, b.Last_Day, h.Chain_Name, h.Category, b.Price, ro.Room_Number
+FROM Renting b 
+JOIN Customer c ON c.Customer_ID = b.Customer_ID
+JOIN Room ro ON b.Room_ID = ro.Room_ID
+JOIN Hotel h ON b.Hotel_ID = h.Hotel_ID;
+
+-- Create Profile view
+CREATE VIEW Profile_View AS
+SELECT 
+  e.SIN, 
+  e.Full_Name, 
+  e.Address,
+  e.Position,
+  h.Chain_Name
+FROM Employee e JOIN Hotel h ON e.Hotel_ID = h.Hotel_ID;     
